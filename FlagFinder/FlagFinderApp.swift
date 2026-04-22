5e47e732a53040af2dcb46dc8b4c138ef8224be1
Luke Sutton <lmsutton31@gmai.com>
2026-04-22 12:35:54 -0400

    Initial Commit

---
+++ FlagFinder/FlagFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlagFinderApp.swift
+//  FlagFinder
+//
+//  Created by Luke Sutton on 4/22/26.
+//
+
+import SwiftUI
+
+@main
+struct FlagFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
